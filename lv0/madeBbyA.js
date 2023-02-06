389d61b1ed4b5b1615628a89f1045e1f7a242c48
HoneyB <qudgus9601@naver.com>
2023-02-07 03:53:22 +0900

    2023-02-07

--- lv0/madeBbyA.js
+++ lv0/madeBbyA.js
@@ -1,9 +1,6 @@
 function solution(before, after) {
   // 순서를 바꾸기만 하면 되니까
   // 결국엔 모든 요소만 가지고 있으면 된다.
-
-  console.log(before.split("").sort());
-  console.log(after.split("").sort());
   return before.split("").sort().join("") === after.split("").sort().join("")
     ? 1
     : 0;
